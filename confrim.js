85981839185bfdaca8bc72e362a3ea528e481a04
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2023-06-08 10:01:31 -0500

    portada con confirm

--- confrim.js
+++ confrim.js
@@ -1,1 +1,6 @@
 confirm("¿Estás listo para adentrarte en la oscuridad y enfrentar tu destino?");    
+function comenzar(perfecto,comenzemos){
+    console.log("Start" + perfecto+ "" +comenzemos);
+    alert("Estas Listo?"+ perfecto+ "" +comenzemos)
+}
+
