3cfdeb6be210b668df33a09e44b820505a6e8742
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2023-06-14 17:16:37 -0500

    start

---
+++ start/confrim.js
@@ -0,0 +1,8 @@
+confirm("¿Estás listo para adentrarte en la oscuridad y enfrentar tu destino?");    
+function comenzar(perfecto,comenzemos){
+    console.log("Start" + perfecto+ "" +comenzemos);
+    alert("Estas Listo?"+ perfecto+ "" +comenzemos)
+}
+function PlayAudio(){
+    document.getElementById("musica1").play();
+}
