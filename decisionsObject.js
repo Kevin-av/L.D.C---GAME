e9e9176b8c9d5c54fb234c73d87dbb7573727b4d
jeremy <116098948+MiguelCriollo@users.noreply.github.com>
2023-06-29 09:55:42 -0500

    Objeto Decisiones Beta

---
+++ decisionsObject.js
@@ -0,0 +1,51 @@
+export const DECISIONS={
+    C0:{
+        name: "Prologo",
+        backgroundLink: "imagenFondo.jpg",
+        path:{
+
+        }
+    },
+    C1:{
+        name: "Limbo",
+        backgroundLink: "imagenFondo.jpg",
+        path:{
+            
+        }
+    },
+    C2:{
+        name: "Lujuria",
+        backgroundLink: "imagenFondo.jpg",
+        path:{
+            
+        }
+    },
+    C3:{
+        name: "Gula",
+        backgroundLink: "imagenFondo.jpg",
+        path:{
+            
+        }
+    },
+    C5:{
+        name: "Avaricia",
+        backgroundLink: "imagenFondo.jpg",
+        path:{
+            
+        }
+    },
+    C6:{
+        name: "Ira",
+        backgroundLink: "imagenFondo.jpg",
+        path:{
+            
+        }
+    },
+    C7:{
+        name: "Herejia",
+        backgroundLink: "imagenFondo.jpg",
+        path:{
+            
+        }
+    }
+}
